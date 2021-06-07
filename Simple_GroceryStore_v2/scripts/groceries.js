974821b8d077722256f0c303f1e39614e61afc62
Jingyi Pan <pjylost@gmail.com>
2021-06-06 23:15:33 -0400

    add version2 of gorcery store

---
+++ Simple_GroceryStore_v2/scripts/groceries.js
@@ -0,0 +1,155 @@
+	
+// Array of products, each product is an object with different fieldset
+// A set of ingredients should be added to products		 
+
+var products = [
+	{
+		name: "brocoli",
+		dairyFree: true,
+		nutsFree: true,
+		price: 1.99,
+		organic: true
+	},
+	{
+		name: "ginger",
+		dairyFree: true,
+		nutsFree: true,
+		price: 2.11,
+		organic: true
+	},
+	{
+		name: "bread",
+		dairyFree: true,
+		nutsFree: true,
+		price: 2.35,
+		organic: false
+	},
+	{
+		name: "cabbage",
+		dairyFree: true,
+		nutsFree: true,
+		price: 3.33,
+		organic: true
+	},
+	{
+		name: "chocolate milk 2L",
+		dairyFree: false,
+		nutsFree: true,
+		price: 4.00,
+		organic: false
+	},
+	{
+		name: "milk 2L",
+		dairyFree: false,
+		nutsFree: true,
+		price: 4.33,
+		organic: false
+	},
+	{
+		name: "cereal",
+		dairyFree: false,
+		nutsFree: false,
+		price: 5.00,
+		organic: false
+	},
+	{
+		name: "salmon",
+		dairyFree: false,
+		nutsFree: true,
+		price: 10.00,
+		organic: false
+	},
+	{
+		name: "chick legs 1kg",
+		dairyFree: true,
+		nutsFree: true,
+		price: 10.33,
+		organic: true
+	},
+	{
+		name: "chicken breast  1kg",
+		dairyFree: true,
+		nutsFree: true,
+		price: 11.00,
+		organic: true
+	},
+	{
+		name: "ground beef  1kg",
+		dairyFree: true,
+		nutsFree: true,
+		price: 11.33,
+		organic: false
+	},
+	{
+		name: "pork back ribs  1kg",
+		dairyFree: true,
+		nutsFree: true,
+		price: 12.00,
+		organic: true
+	},
+	{
+		name: "ground pork  1kg",
+		dairyFree: true,
+		nutsFree: true,
+		price: 12.33,
+		organic: false
+	},
+	{
+		name: "AAA Angus Beef Steak 1kg",
+		dairyFree: true,
+		nutsFree: true,
+		price: 17.00,
+		organic: true
+	}
+];
+	
+
+
+// given restrictions provided, make a reduced list of products
+// prices should be included in this list, as well as a sort based on price
+
+function restrictListProducts(prods, preferDairyFree, preferNutsFree, preferOrganic) {
+	let products_filtered = [];
+	for (let i=0; i<prods.length; i+=1) {
+		let shallAdd = true;
+		if (preferDairyFree && (prods[i].dairyFree == false))
+		{
+			shallAdd = false;
+		}
+		if (preferNutsFree && !prods[i].nutsFree)
+		{
+			shallAdd = false;
+		}
+		if (preferOrganic && !prods[i].organic)
+		{
+			shallAdd = false;
+		}
+
+		if (shallAdd)
+		{
+			products_filtered.push(prods[i]);
+		}
+		// if ((prods[i].dairyFree == preferDairyFree) && (prods[i].nutsFree == preferNutsFree) && (prods[i].organic == preferOrganic)){
+		// 	products_filtered.push(prods[i]);
+		// }
+		// else if ((restriction == "NutsFree") && (prods[i].nutsFree == true) && (prods[i].organic == preferOrganic)){
+		// 	products_filtered.push(prods[i]);
+		// }
+		// else if ((restriction == "None")  && (prods[i].organic == preferOrganic)){
+		// 	products_filtered.push(prods[i]);
+		// }
+	}
+	return products_filtered;
+}
+
+// Calculate the total price of items, with received parameter being a list of products
+function getTotalPrice(chosenProducts) {
+	let totalPrice = 0;
+	for (let i=0; i<products.length; i+=1) {
+		if (chosenProducts.indexOf(products[i].name) > -1){
+			totalPrice += products[i].price;
+		}
+	}
+	totalPrice = totalPrice.toFixed(2);
+	return totalPrice;
+}
---
+++ Simple_GroceryStore_v2/scripts/main.js
@@ -0,0 +1,216 @@
+
+
+// This function is called when any of the tab is clicked
+// It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
+
+function openInfo(id, tabName) {
+
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+		if (tablinks[i].id == id && i != 0)
+		{
+			// for (j = 0; j < i; j++) 
+			// {
+				tablinks[i - 1].disabled = false;
+		}
+	}
+
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(tabName).style.display = "block";
+	document.getElementById(id).className += " active";
+
+}
+
+function confirm(evt)
+{
+	var tab;
+	console.log(evt.currentTarget.id);
+	switch (evt.currentTarget.id)
+	{
+		case "confirm1":
+			tab = document.getElementById('navPref');
+			console.log(tab);
+			break;
+		case "confirm2":
+			tab = document.getElementById('navSelectPro');
+			break;
+		case "confirm3":
+			tab = document.getElementById('navReview');
+			break;
+		case "confirm4":
+			tab = document.getElementById('navChooseDeliv');
+			break;
+		case "confirm5":
+			console.log(evt.currentTarget.id);
+			tab = document.getElementById('navCheckout');
+			break;
+	}
+	console.log(tab);
+	tab.disabled = false;
+	tab.click();
+	
+}
+	
+// generate a checkbox list from a list of products
+// it makes each product name as the label for the checkbos
+
+function populateListProductChoices() {
+    //var s1 = document.getElementById(slct1);
+    var s2 = document.getElementById("displayProduct");
+	
+	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
+    s2.innerHTML = "";
+		
+	let checkboxDairyFree = document.getElementById("DairyFree");
+	let checkboxNutsFree = document.getElementById("NutsFree");
+	let checkboxOrganic = document.getElementById("organic");
+	//console.log(checkboxPref.checked);
+	// obtain a reduced list of products based on restrictions
+    var optionArray = restrictListProducts(products, checkboxDairyFree.checked, checkboxNutsFree.checked, checkboxOrganic.checked);
+	// if (document.getElementById("showAll").checked)
+	// {
+	// 	optionArray = products;
+	// }
+
+	// for each item in the array, create a checkbox element, each containing information such as:
+	// <input type="checkbox" name="product" value="Bread">
+	// <label for="Bread">Bread/label><br>
+
+	// var productTable = document.getElementById("productTable1");
+	// console.log(productTable);
+	for (i = 0; i < optionArray.length; i++) {
+		var productName = optionArray[i].name;
+		console.log(productName);
+		// create the checkbox and add in HTML DOM
+		
+		// let newRow = productTable.insertRow(-1);
+
+		// // Insert a cell at the end of the row
+		// let newCell = newRow.insertCell();
+
+		// // Append a text node to the cell
+		// let newText = document.createTextNode(productName);
+		// newCell.appendChild(newText);
+
+		let row = document.createElement("div");
+		row.style.display = "table-row";
+		var checkbox = document.createElement("input");
+		checkbox.style.display = "table-cell";
+		//checkbox.style.width = "5px";
+		//checkbox.className = "itemRow";
+		checkbox.type = "checkbox";
+		checkbox.name = "product";
+		checkbox.id = productName;
+		checkbox.value = productName;
+		//s2.appendChild(checkbox);
+		row.appendChild(checkbox);
+		
+		// create a label for the checkbox, and also add in HTML DOM
+		var label = document.createElement('label')
+		label.htmlFor = productName;
+		let nameSpan = document.createElement("span");
+		nameSpan.style.display = "table-cell";
+		nameSpan.style.width = "250px";
+		//nameSpan.style.width = "200px";
+		nameSpan.appendChild(document.createTextNode(productName));
+		label.appendChild(nameSpan);
+
+		let priceSpan = document.createElement("span");
+		priceSpan.style.display = "table-cell";
+		priceSpan.style.width = "100px";
+		//priceSpan.style.width = "5px";
+		//priceSpan.style.float = "left";
+		priceSpan.appendChild(document.createTextNode("$" + optionArray[i].price));
+		label.appendChild(priceSpan);
+
+		let organicSpan = document.createElement("span");
+		organicSpan.style.display = "table-cell";
+		organicSpan.style.width = "200px";
+		//organicSpan.style.width = "10px";
+		organicSpan.appendChild(document.createTextNode("organic: " + (optionArray[i].organic?"yes":"no")));
+		label.appendChild(organicSpan);
+		//label.appendChild(document.createTextNode(productName + "\xa0 \xa0 \xa0 $" 
+		//+ optionArray[i].price + "\xa0 \xa0 \xa0 organic: " + (optionArray[i].organic?"yes":"no")));
+		//s2.appendChild(label);
+		row.appendChild(label);
+		
+		// create a breakline node and add in HTML DOM
+		s2.appendChild(row);
+		//s2.appendChild(document.createElement("br"));    
+	}
+}
+	
+// This function is called when the "Add selected items to cart" button in clicked
+// The purpose is to build the HTML to be displayed (a Paragraph) 
+// We build a paragraph to contain the list of selected items, and the total price
+
+function selectedItems(){
+	
+	var ele = document.getElementsByName("product");
+	var chosenProducts = [];
+	
+	var c = document.getElementById('displayCart');
+	c.innerHTML = "";
+	
+	let tableContent = "<table><tr><th style='width: 250px; text-align: left;'>Product Name  </th><th>Product Price  </th></tr>";
+
+	// build list of selected item
+	var para = document.createElement("P");
+	//para.innerHTML = "You selected : ";
+	//para.appendChild(document.createElement("br"));
+	for (i = 0; i < ele.length; i++) { 
+		if (ele[i].checked) {
+			para.appendChild(document.createTextNode(ele[i].value));
+			para.appendChild(document.createElement("br"));
+			chosenProducts.push(ele[i].value);
+			for (j = 0; j < products.length; j++)
+			{
+				if (products[j].name == ele[i].value)
+				{
+					tableContent += "<tr><td>" + ele[i].value + "</td><td>" + products[j].price + "</td></tr>";
+					break;
+				}
+			}
+		}
+	}
+
+	tableContent += "</table>";
+	c.innerHTML = tableContent;
+	
+	//tableDiv.innerHTML = tableContent;
+
+		
+	// // add paragraph and total price
+	// c.appendChild(para);
+	// let hr = document.createElement("hr");
+	// c.appendChild(hr);
+	price_h3 = document.createElement("h3")
+	price_h3.innerHTML = "The Total Price is $" + getTotalPrice(chosenProducts);
+	c.appendChild(price_h3);
+	//c.appendChild(document.createTextNode("Total Price is " + getTotalPrice(chosenProducts)));
+	populateCheckoutBoard();
+}
+
+function populateCheckoutBoard()
+{
+	document.getElementById("finalCart").innerHTML = document.getElementById("displayCart").innerHTML;
+	let del = document.getElementsByName("timeSlot");
+	for (i = 0; i < del.length; i++)
+	{
+		if (del[i].checked)
+		{
+			document.getElementById("finalDeliveryTime").innerHTML = del[i].value.split(";;")[0];
+			document.getElementById("finalDeliveryPrice").innerHTML = del[i].value.split(";;")[1];
+			break;
+		}
+	}
+	
+}
